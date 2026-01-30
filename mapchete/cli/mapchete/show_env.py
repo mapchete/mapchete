4af6e7978e350c64bdc58e215dbcd40ce5e2a862
Joachim Ungar <joachim.ungar@eox.at>
2026-01-30 11:56:25 +0100

    CLI: add show-env command (#723)
    * add show-env CLI command
    
    * update license year
    
    * update uv.lock
    
    * provide more information when printing env options
    
    * add info that MAPCHETE_IO_RETRY_EXCEPTIONS cannot be set via environment
    
    * Revert "add info that MAPCHETE_IO_RETRY_EXCEPTIONS cannot be set via environment"
    
    This reverts commit eae6e591e469dae40e5cf15ca14e622b9368f6f1.
    
    * try to release aiobotocore constraint
    
    * Bump distributed from 2025.12.0 to 2026.1.1 (#716)
    
    * Bump distributed from 2025.12.0 to 2026.1.1
    
    Bumps [distributed](https://github.com/dask/distributed) from 2025.12.0 to 2026.1.1.
    - [Release notes](https://github.com/dask/distributed/releases)
    - [Changelog](https://github.com/dask/distributed/blob/main/docs/release-procedure.md)
    - [Commits](https://github.com/dask/distributed/compare/2025.12.0...2026.1.1)
    
    ---
    updated-dependencies:
    - dependency-name: distributed
      dependency-version: 2026.1.1
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: sync uv.lock and conda recipe (tests passed)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    * Bump dask from 2025.12.0 to 2026.1.1 (#718)
    
    * Bump dask from 2025.12.0 to 2026.1.1
    
    Bumps [dask](https://github.com/dask/dask) from 2025.12.0 to 2026.1.1.
    - [Release notes](https://github.com/dask/dask/releases)
    - [Changelog](https://github.com/dask/dask/blob/main/docs/release-procedure.md)
    - [Commits](https://github.com/dask/dask/compare/2025.12.0...2026.1.1)
    
    ---
    updated-dependencies:
    - dependency-name: dask
      dependency-version: 2026.1.1
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: sync uv.lock and conda recipe (tests passed)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    * chore: sync uv.lock and conda recipe (tests passed)
    
    * move single raster output in_memory setting to global mapchete settings
    
    * fix options model
    
    * fix test coverage
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ mapchete/cli/mapchete/show_env.py
@@ -0,0 +1,38 @@
+"""CLI to format drivers."""
+
+from enum import Enum
+from typing import Literal
+
+import click
+
+from mapchete.settings import MapcheteOptions, IORetrySettings, GDALHTTPOptions
+
+
+@click.command(help="List all possible environment settings.")
+def show_env():
+    """List all possible environment settings."""
+    for setting_model in [MapcheteOptions, IORetrySettings, GDALHTTPOptions]:
+        env_prefix = setting_model.model_config.get("env_prefix", "")
+        for field_name, field_info in setting_model.model_fields.items():
+            if field_name == "exceptions":
+                continue
+            # field_info contains metadata like annotation (type) and default
+            annotation = field_info.annotation
+            default_value = field_info.default
+
+            # Check for Literal
+            if hasattr(annotation, "__origin__") and annotation.__origin__ is Literal:
+                options_str = f" ({', '.join(annotation.__args__)})"
+
+            # Check for Enum
+            elif isinstance(annotation, type) and issubclass(annotation, Enum):
+                options_str = (
+                    f" ({', '.join([str(member.value) for member in annotation])})"
+                )
+                default_value = field_info.default.name
+            else:
+                options_str = ""
+
+            click.echo(
+                f"{env_prefix}{field_name.upper()}{options_str}: {default_value}"
+            )
