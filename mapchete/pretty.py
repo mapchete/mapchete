5b3c32e566d1aa98f1631fa0977231271cf15686
Joachim Ungar <joachim.ungar@eox.at>
2026-09-04 08:22:41 +0200

    add MPath.local_cache() and pretty_numbers() (#830)
    * add MPath.local_copy() method; properly raise FileNotFoundError on MPath.walk() and MPath.ls()
    * add pretty.pretty_number() and decimal flag to pretty.pretty_bytes()

--- mapchete/pretty.py
+++ mapchete/pretty.py
@@ -1,33 +1,54 @@
-def pretty_bytes(count: float, round_value: int = 2) -> str:
+def pretty_bytes(count: float, round_value: int = 2, decimal: bool = False) -> str:
     """Return human readable bytes."""
     out = ""
 
-    for measurement in [
-        "bytes",
-        "KiB",
-        "MiB",
-        "GiB",
-        "TiB",
-        "PiB",
-        "EiB",
-        "ZiB",
-        "YiB",
-    ]:
+    if decimal:
+        measurements = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+        divisor = 1000.0
+    else:
+        measurements = [
+            "bytes",
+            "KiB",
+            "MiB",
+            "GiB",
+            "TiB",
+            "PiB",
+            "EiB",
+            "ZiB",
+            "YiB",
+        ]
+        divisor = 1024.0
+
+    for measurement in measurements:
         out = f"{round(count, round_value)} {measurement}"
-        if count < 1024.0:
+        if count < divisor:
             break
-        count /= 1024.0
+        count /= divisor
 
     return out
 
 
-def pretty_seconds(elapsed_seconds: float, round_value: int = 3):
+def pretty_seconds(elapsed_seconds: float, round_value: int = 3) -> str:
     """Return human readable seconds."""
     minutes, seconds = divmod(elapsed_seconds, 60)
     hours, minutes = divmod(minutes, 60)
     if hours:
+        if hours > 1000:
+            return f"{pretty_number(hours, round_value=0)} hours"
         return f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
     elif minutes:
         return f"{int(minutes)}m {int(seconds)}s"
     else:
         return f"{round(seconds, round_value)}s"
+
+
+def pretty_number(number: float, round_value: int = 2) -> str:
+    """Return human readable, rounded number."""
+    out = ""
+
+    for unit in ["", " thousand", " million", " billion", " trillion", " quadrillion"]:
+        out = f"{round(number, round_value)}{unit}"
+        if number < 1000.0:
+            break
+        number /= 1000.0
+    return out
--- test/test_pretty.py
+++ test/test_pretty.py
@@ -4,23 +4,23 @@ from mapchete.pretty import pretty_bytes, pretty_seconds
 
 
 @pytest.mark.parametrize(
-    "bytes,string",
+    "bytes,string,decimal",
     [
-        (2_000, "KiB"),
-        (2_000_000, "MiB"),
-        (2_000_000_000, "GiB"),
+        (2_000, "KiB", False),
+        (2_000_000, "MiB", False),
+        (2_000_000_000, "GiB", False),
+        (2_000, "KB", True),
+        (2_000_000, "MB", True),
+        (2_000_000_000, "GB", True),
     ],
 )
-def test_pretty_bytes(bytes, string):
-    assert string in pretty_bytes(bytes)
+def test_pretty_bytes(bytes, string, decimal):
+    assert string in pretty_bytes(bytes, decimal=decimal)
 
 
 @pytest.mark.parametrize(
     "seconds,string",
-    [
-        (61, "m"),
-        (3601, "h"),
-    ],
+    [(61, "m"), (3601, "h"), (1000000000, "thousand hours")],
 )
 def test_pretty_seconds(seconds, string):
     assert string in pretty_seconds(seconds)
